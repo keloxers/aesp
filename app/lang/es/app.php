d7d58e9fd970d8158aeff0cf46f363b186bb8311
Miguel Mendez <keloxers@gmail.com>
2015-03-26 21:23:15 -0300

    actualizacion2

--- app/lang/es/app.php
+++ app/lang/es/app.php
@@ -14,25 +14,25 @@ return array(
 
 	'title_home' => "Agrupacion Empleados Salud Pública",
 
-	'email'		=>	"<strong>comercial@synmisiones.com</strong>",
+	'email'		=>	"<strong>info@aespmisiones.com</strong>",
 
-	'telefono'		=>	"<strong>+54 0376 4439326</strong>",
+	'telefono'		=>	"<strong>+54 0376 XXXXXXX</strong>",
 
-	'seguinos_facebook'		=>	"https://facebook.com/pages/SN-Servicios/1564714993750159?ref=hl S&N Servicios",
+	'seguinos_facebook'		=>	"https://www.facebook.com/AESPMnes",
 
-	'seguinos_twitter'		=>	"https://twitter.com/synservicios",
+	'seguinos_twitter'		=>	"https://twitter.com/AESPMnes",
 
 	'seguinos_googleplus'		=>	"http://plus.google.com/u/0/116409445805080169378/posts",
 
 	'seguinos_instagram'		=>	"",
 
-	'seguinos_youtube'		=>	"http://www.youtube.com/channel/UCWTCkRQ6MUXuSK1TXp0GI_w",
+	'seguinos_youtube'		=>	"",
 
 	'direccion'		=>	"Posadas, Misiones AR",
 
-	'contacto'		=>	"+54 0376 4439326 <br><a href='mailto:info@synmisiones.com'>info@synmisiones.com</a>",
+	'contacto'		=>	"+54 0376 XXXXXXX <br><a href='mailto:info@aespmisiones.com'>info@aespmisiones.com</a>",
 
-	'title_food' => "S&N Misiones",
+	'title_food' => "AESP Misiones",
 
 ''		=>	"",
 ''		=>	"",
