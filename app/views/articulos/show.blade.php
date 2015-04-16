d789af973f24f2dd0643b965066b8bcfe7de0da1
Miguel Mendez <keloxers@gmail.com>
2015-04-15 21:02:30 -0300

    uploads

--- app/views/articulos/show.blade.php
+++ app/views/articulos/show.blade.php
@@ -63,6 +63,8 @@
 					<span class="pi-dropcap"></span>{{$articulo->texto}}
 				</p>
 
+				<?php $url_seo = $articulo->url_seo; 
+				?>
 
 
 				<!-- Box -->
@@ -167,21 +169,8 @@
 								<h4 class="pi-has-bg pi-weight-700 pi-uppercase pi-letter-spacing pi-margin-bottom-20">
 									Comentarios
 								</h4>
-								<div id="disqus_thread"></div>
-								<script type="text/javascript">
-									/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-									var disqus_shortname = 'aespmisiones'; // required: replace example with your forum shortname
 
-									/* * * DON'T EDIT BELOW THIS LINE * * */
-									(function() {
-										var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-										dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-										(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-									})();
-								</script>
-								<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-								<a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
-
+								<div class="fb-comments" data-href="http://www.http://aespmisiones.com/articulos/show/{{$url_seo}}" data-numposts="20" data-colorscheme="light"></div>
 
 				<?php } ?>
 
